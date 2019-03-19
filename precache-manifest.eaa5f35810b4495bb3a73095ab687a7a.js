27ab3d01e5724233f134aeea2f12d194311b8636
christophrus <christophrus@protonmail.com>
2019-03-19 01:39:01 +0100

    Updates

---
+++ precache-manifest.eaa5f35810b4495bb3a73095ab687a7a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fcc-pomodoro-clock/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "afbd6cc7ff47ac038e56",
+    "url": "/fcc-pomodoro-clock/static/css/main.85ac35f9.chunk.css"
+  },
+  {
+    "revision": "ec7b801c7c9eecf06fdf",
+    "url": "/fcc-pomodoro-clock/static/js/runtime~main.a052a505.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fcc-pomodoro-clock/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "aae185c1e5e6eb950df5",
+    "url": "/fcc-pomodoro-clock/static/js/2.26698ea9.chunk.js"
+  },
+  {
+    "revision": "afbd6cc7ff47ac038e56",
+    "url": "/fcc-pomodoro-clock/static/js/main.778805fe.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fcc-pomodoro-clock/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fcc-pomodoro-clock/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fcc-pomodoro-clock/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "aae185c1e5e6eb950df5",
+    "url": "/fcc-pomodoro-clock/static/css/2.31552fdd.chunk.css"
+  },
+  {
+    "revision": "858d1237f2bc61bad4fdf0d7d316348d",
+    "url": "/fcc-pomodoro-clock/index.html"
+  }
+];
